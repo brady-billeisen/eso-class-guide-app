54a872cad616483de42853c8d8a625de5e320cf7
brady <bradybill36@gmail.com>
2023-05-31 18:34:40 -0500

    changed code to get data based off the options selected, still has some bugs, and then changed the css so that the ul on the nav bar appears at the right side instead of the left

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,34 +2,125 @@ var youtubeApiKey = 'AIzaSyD72xaN7Me4CqXheV39Q-OkMFk86DYRX90';
 
 var subreddit = 'elderscrollsonline';
 var searchParam = '';
+var searchPlatform = '';
 
 
-function getRedditData() {
-    if (searchParam === '-- Select a Class --') {
+// function getRedditData() {
+//     if (searchParam === '-- Select a Class --') {
+//         return;
+//     }
+//     var redditUrl = `https://www.reddit.com/r/${subreddit}/search.json?q=${searchParam}&restrict_sr=1&sort=top&limit=25`;
+//     fetch(redditUrl)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//             var resultsHead = $('<h2>').text('Top results on Reddit for ' + searchParam + ':');
+//             $('#results').append(resultsHead);
+//             for (var i = 0; i < data.data.children.length; i++) {
+//                 var card = $('<div>').addClass('card mb-3');
+//                 var cardBody = $('<div>').addClass('card-body');
+//                 var anchor = $('<a>').attr('href', 'https://reddit.com' + data.data.children[i].data.permalink);
+//                 var title = $('<h2>').text(data.data.children[i].data.title);
+//                 var thumbnail = $('<img>').attr('src', data.data.children[i].data.thumbnail);
+//                 var author = $('<p>').text('By: ' + data.data.children[i].data.author);
+//                 card.append(cardBody);
+//                 cardBody.append(anchor, thumbnail, author);
+//                 anchor.append(title);
+//                 $('#results').append(card);
+//             }
+//         });
+// }
+
+
+// function getYoutubeData() {
+//     if (searchParam === '-- Select a Class --') {
+//         return;
+//     }
+//     var youtubeUrl = `https://www.googleapis.com/youtube/v3/search?key=${youtubeApiKey}&q=${'Elder Scrolls Online ' + searchParam}&maxResults=25&type=video`;
+//     fetch(youtubeUrl)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//             for (var i = 0; i < data.items.length; i++) {
+//                 var card = $('<div>').addClass('card mb-3');
+//                 var cardBody = $('<div>').addClass('card-body');
+//                 var iframe = $('<iframe>').attr({
+//                     'src': 'https://www.youtube.com/embed/' + data.items[i].id.videoId,
+//                     'id': 'ytplayer',
+//                     'type': 'text/html',
+//                     'frameborder': '0',
+//                     'width': '640',
+//                     'height': '360'
+//                   });
+//                 card.append(cardBody);
+//                 cardBody.append(iframe);
+//                 $('#results').append(card);
+//             }
+//         });
+// }
+
+
+function getData() {
+    if (searchPlatform === '-- Select a Platform --' || searchParam === '-- Select a Class --') {
+        clearResults();
         return;
     }
-    var redditUrl = `https://www.reddit.com/r/${subreddit}/search.json?q=${searchParam}&restrict_sr=1&sort=top&limit=25`;
-    fetch(redditUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            var resultsHead = $('<h2>').text('Top results for ' + searchParam + ':');
-            $('#results').append(resultsHead);
-            for (var i = 0; i < data.data.children.length; i++) {
-                var card = $('<div>').addClass('card mb-3');
-                var cardBody = $('<div>').addClass('card-body');
-                var anchor = $('<a>').attr('href', 'https://reddit.com' + data.data.children[i].data.permalink);
-                var title = $('<h2>').text(data.data.children[i].data.title);
-                var thumbnail = $('<img>').attr('src', data.data.children[i].data.thumbnail);
-                var author = $('<p>').text('By: ' + data.data.children[i].data.author);
-                card.append(cardBody);
-                cardBody.append(anchor, thumbnail, author);
-                anchor.append(title);
-                $('#results').append(card);
-            }
-        });
+
+
+    if (searchPlatform === 'Reddit') {
+        var redditUrl = `https://www.reddit.com/r/${subreddit}/search.json?q=${searchParam}&restrict_sr=1&sort=top&limit=25`;
+        fetch(redditUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                var resultsHead = $('<h2>').text('Top results on Reddit for ' + searchParam + ':');
+                $('#results').append(resultsHead);
+                for (var i = 0; i < data.data.children.length; i++) {
+                    var card = $('<div>').addClass('card mb-3');
+                    var cardBody = $('<div>').addClass('card-body');
+                    var anchor = $('<a>').attr('href', 'https://reddit.com' + data.data.children[i].data.permalink);
+                    var title = $('<h2>').text(data.data.children[i].data.title);
+                    var thumbnail = $('<img>').attr('src', data.data.children[i].data.thumbnail);
+                    var author = $('<p>').text('By: ' + data.data.children[i].data.author);
+                    card.append(cardBody);
+                    cardBody.append(anchor, thumbnail, author);
+                    anchor.append(title);
+                    $('#results').append(card);
+                }
+            });
+    } else if (searchPlatform === 'Youtube') {
+        var youtubeUrl = `https://www.googleapis.com/youtube/v3/search?key=${youtubeApiKey}&q=${'Elder Scrolls Online ' + searchParam}&maxResults=25&type=video`;
+        fetch(youtubeUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                var resultsHead = $('<h2>').text('Top results on Youtube for "Elder Scrolls Online ' + searchParam + '"' + ':');
+                $('#results').append(resultsHead);
+                for (var i = 0; i < data.items.length; i++) {
+                    var card = $('<div>').addClass('card mb-3');
+                    var cardBody = $('<div>').addClass('card-body');
+                    var iframe = $('<iframe>').attr({
+                        'src': 'https://www.youtube.com/embed/' + data.items[i].id.videoId,
+                        'id': 'ytplayer',
+                        'type': 'text/html',
+                        'frameborder': '0',
+                        'width': '640',
+                        'height': '360'
+                    });
+                    card.append(cardBody);
+                    cardBody.append(iframe);
+                    $('#results').append(card);
+                }
+            });
+    }
 }
 
 
@@ -37,10 +128,15 @@ function clearResults() {
     $('#results').empty();
 }
 
+function setPlatform(platform) {
+    searchPlatform = platform;
+    getData();
+}
+
 
 function setParam(param) {
     searchParam = param;
-    getRedditData();
+    getData();
 }
 
 
@@ -49,4 +145,12 @@ $('#eso-class').change(function (event) {
     var param = $(this).val();
     clearResults();
     setParam(param);
+})
+
+
+$('#platform').change(function (event) {
+    event.preventDefault();
+    var platform = $(this).val();
+    clearResults();
+    setPlatform(platform);
 })
